cb5bef121ff3e8718aff54b985363fcedf350fda
hritikxyadav <hritikyadav152@gmail.com>
2026-02-02 23:06:45 +0530

    updates

---
+++ Day_01/q2.c
@@ -0,0 +1,22 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    *returnSize = 2;
+    
+    int* result = (int*)malloc(2 * sizeof(int));
+    if (result == NULL) return NULL;
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    
+    return NULL;
+}
